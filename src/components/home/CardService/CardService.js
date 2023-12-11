e8cfcadada059344708c5daa5855741188b43bcc
Novan Junaedi <junaedinovan98@gmail.com>
2023-12-11 22:09:54 +0700

    update section services

--- src/components/home/CardService/CardService.js
+++ src/components/home/CardService/CardService.js
@@ -1,11 +1,12 @@
 import Link from 'next/link';
 import { FaArrowRight } from 'react-icons/fa';
 
-const CardService = ({ title, description, link }) => (
+const CardService = ({ title, description, link, icon }) => (
   <div className="card border-0 shadow rounded-4 h-100">
     <div className="card-body">
-      <h5 className="card-title display-6 my-3">{title}</h5>
-      <p className="text-dark">{description}</p>
+      {icon}
+      <h5 className="card-title my-3">{title}</h5>
+      <p className="text-muted">{description}</p>
       {link && (
         <Link className="link link-dark my-3 float-end" href={link}>Selengkapnya <FaArrowRight /></Link>
       )}
--- src/components/home/SectionServices/SectionServices.js
+++ src/components/home/SectionServices/SectionServices.js
@@ -1,25 +1,30 @@
+import { FaBoxOpen, FaChartLine, FaPeopleArrows, FaSearch } from 'react-icons/fa';
 import CardService from '../CardService/CardService';
 
 const SectionServices = () => {
   const services = [
     {
+      icon: <FaSearch size="3em" />,
       title: 'Search Engine Optimization',
-      description: 'Naikkan peringkat, raih visibilitas maksimal. Dengan layanan SEO kami, hadirkan bisnis Anda ke puncak hasil pencarian. Optimalkan online presence Anda dan tingkatkan trafik organik. Sukses di dunia digital dimulai dengan strategi SEO yang tepat. Bersama, kita tingkatkan posisi dan hadirkan keberhasilan digital bagi bisnis Anda!',
+      description: 'Naikkan peringkat, raih visibilitas maksimal. Dengan layanan SEO kami, hadirkan bisnis Anda ke puncak hasil pencarian.',
       link: '/blogs/memahami-seo-panduan-singkat-untuk-meraih-posisi-teratas-di-mesin-pencari',
     },
     {
+      icon: <FaChartLine size="3em" />,
       title: 'Performance Marketing',
-      description: 'Tingkatkan eksposur, capai hasil maksimal. Manfaatkan kekuatan iklan digital dengan layanan kami. Targetkan audiens tepat, optimalkan konversi. Bisnis Anda layak mendapatkan sorotan, dan kami di sini untuk memastikan itu terjadi. Aktifkan potensi penuh bisnis Anda melalui kampanye iklan yang efektif dan relevan!',
+      description: 'Tingkatkan eksposur, capai hasil maksimal. Manfaatkan kekuatan iklan digital dengan layanan kami. Targetkan audiens tepat, optimalkan konversi.',
       link: '',
     },
     {
+      icon: <FaPeopleArrows size="3em" />,
       title: 'Social Media Management',
-      description: 'Optimalkan kehadiran digital Anda dengan Social Media Management kami. Dari strategi konten hingga interaksi autentik, kami membantu menciptakan narasi yang tak terlupakan. Manfaatkan potensi media sosial untuk keberhasilan yang signifikan bersama kami.',
+      description: 'Dari strategi konten hingga interaksi autentik, kami membantu menciptakan narasi tak terlupakan. Manfaatkan potensi media sosial untuk keberhasilan signifikan.',
       link: '',
     },
     {
+      icon: <FaBoxOpen size="3em" />,
       title: 'Product Development',
-      description: 'Jelajahi potensi tanpa batas melalui inovasi produk bersama kami. Dengan pendekatan terstruktur dan fokus pada keunggulan, tim pengembangan produk kami siap membawa visi Anda menjadi kenyataan. Hadirkan perubahan positif melalui solusi yang dirancang khusus untuk menciptakan pengalaman produk tak terlupakan.',
+      description: 'Dengan pendekatan terstruktur dan fokus pada keunggulan, tim pengembangan produk kami siap membawa visi Anda menjadi kenyataan.',
       link: '',
     },
   ];
@@ -31,8 +36,9 @@ const SectionServices = () => {
 
         <div className="row my-4">
           {services.map((service, index) => (
-            <div className="col-md-6 p-3" key={index}>
+            <div className="col-md-6 col-xl-3 p-2" key={index}>
               <CardService
+                icon={service.icon}
                 title={service.title}
                 description={service.description}
                 link={service.link}
