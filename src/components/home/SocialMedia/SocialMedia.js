df2d90aa4e34f9daccdbe520266a154513973898
Novan Junaedi <junaedinovan98@gmail.com>
2024-01-19 17:48:24 +0700

    update linkedin link

--- src/components/home/SocialMedia/SocialMedia.js
+++ src/components/home/SocialMedia/SocialMedia.js
@@ -10,7 +10,7 @@ const SocialMedia = () => (
     <a className="link-light" href="https://facebook.com/lestarymedia" target="_blank" rel="noopener noreferrer">
       <FaFacebook size={20} title="Facebook" />
     </a>
-    <a className="link-light" href="https://linkedin.com/in/lestarymedia" target="_blank" rel="noopener noreferrer">
+    <a className="link-light" href="https://linkedin.com/company/lestarymedia" target="_blank" rel="noopener noreferrer">
       <FaLinkedin size={20} title="LinkedIn" />
     </a>
   </div>
